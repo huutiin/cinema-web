176fb368448d1f892ae65980a54052b9b56b3e8f
Huu Tin Nguyen <tinnguen123@gmail.com>
2025-06-15 02:32:48 +0700

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,7 +66,7 @@ export default function Header() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link
             to="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse border border-gray-300 dark:border-gray-600"
+            className="flex items-center space-x-3 rtl:space-x-reverse rounded-lg border-solid border-2 pr-3 border-gray-300 dark:border-gray-600"
           >
             <img src={logo} className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
